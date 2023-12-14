8c59c56fa33f9d11122df5dd89168f64bb66e8d6
uelkariuki <cratzuel@gmail.com>
2023-12-14 22:42:22 +0300

    Add SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
-DROP PROCEDURE ComputeAverageScoreForUser;
+
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
