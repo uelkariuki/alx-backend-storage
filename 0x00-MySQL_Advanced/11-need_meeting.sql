902da0461beb8e075b5c9cdfe5e0f6c85c69e08c
uelkariuki <cratzuel@gmail.com>
2023-12-15 01:59:17 +0300

    Implement alternative to reduce current date by one month

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 
 
 CREATE VIEW need_meeting AS
-SELECT name FROM students WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+SELECT name FROM students WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < CURDATE() - INTERVAL 1 MONTH);
