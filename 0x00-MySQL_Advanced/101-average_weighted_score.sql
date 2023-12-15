2307116c9b366bedb3d7935606d30f2aab90763e
uelkariuki <cratzuel@gmail.com>
2023-12-15 04:00:05 +0300

    Fix ComputeAverageWeightedScoreForUser stored procedure call required by ComputeAverageWeightedScoreForUsers stored procedure

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,4 +1,30 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE total_weight FLOAT;
+  DECLARE weighted_sum_score FLOAT;
+
+  SELECT SUM(projects.weight) INTO total_weight
+  FROM corrections
+  JOIN projects ON corrections.project_id = projects.id
+  WHERE corrections.user_id = user_id;
+
+  SELECT SUM(corrections.score * projects.weight) INTO weighted_sum_score
+  FROM corrections
+  JOIN projects ON corrections.project_id = projects.id
+  WHERE corrections.user_id = user_id;
+
+  UPDATE users
+  SET average_score = weighted_sum_score / total_weight
+  WHERE id = user_id;
+
+END //
+DELIMITER ;
+
+
+
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
