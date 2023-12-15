a4cd6dcaa18434d07a1c15c69ac08fcb75afd281
uelkariuki <cratzuel@gmail.com>
2023-12-15 03:53:42 +0300

    Fix output error

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,5 +1,4 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
-
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
