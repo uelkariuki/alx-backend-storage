b3e1039982106ff4c41e22367c127e122f8a0b09
uelkariuki <cratzuel@gmail.com>
2023-12-20 22:23:07 +0300

    Rea from Redis and recovering  original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache(object):
@@ -22,3 +22,31 @@ class Cache(object):
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable]):
+        """
+        get method that take a key string argument,
+        and an optional Callable argument named fn
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        elif fn is not None:
+            return fn(value)
+        else:
+            return value
+
+    def get_str(self, key: str) -> str:
+        """Retrieves value of key from Redis and converts it to a str"""
+        value = self.get(key)
+        if value is None:
+            return None
+        else:
+            return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """Retrieves value of key from Redis and converts it to an int"""
+        value = self.get(key)
+        if value:
+            return int(value)
+        return None
