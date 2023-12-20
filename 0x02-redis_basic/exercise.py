97006cbc23d6213bf40f791f561e2c00e76f5e73
uelkariuki <cratzuel@gmail.com>
2023-12-20 23:30:13 +0300

    Implement Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,24 @@ Writing strings to Redis
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """
+    count_calls decorator
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """
+        Wrapper method that also implements how many times
+        the methods of Cache class are called
+        """
+        key = method.__qualname__
+        self._redis.incr(key, 1)
+
+        return method(self, *args, **kwds)
+    return wrapper
 
 class Cache(object):
     """
@@ -17,12 +34,14 @@ class Cache(object):
         self._redis = redis.Redis()
         self._redis.flushdb()  # Flush database to clear old entries
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store method that returns a string"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
 
+    @count_calls
     def get(self, key: str, fn: Optional[Callable] = None):
         """
         get method that take a key string argument,
@@ -36,6 +55,7 @@ class Cache(object):
         else:
             return value
 
+    @count_calls
     def get_str(self, key: str) -> str:
         """Retrieves value of key from Redis and converts it to a str"""
         value = self.get(key)
@@ -44,6 +64,7 @@ class Cache(object):
         else:
             return value.decode('utf-8')
 
+    @count_calls
     def get_int(self, key: str) -> int:
         """Retrieves value of key from Redis and converts it to an int"""
         value = self.get(key)
