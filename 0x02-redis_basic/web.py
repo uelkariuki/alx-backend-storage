7fe7764ae3ca51275c0a08626061619384f6f567
uelkariuki <cratzuel@gmail.com>
2023-12-21 05:32:58 +0300

    Fix the implementation of an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,40 +6,22 @@ Implementing an expiring web cache and tracker
 
 import requests
 import redis
-from typing import Callable
 
 # connecting to local Redis server
 red = redis.Redis()
+count = 0
 
 
-def cache(fn: Callable) -> Callable:
-    """
-    cache decorator to add caching functionality
-    """
-    def wrapper(url):
-        """Wrapper function"""
-        # checking if this page has been cached before
-        if red.get(url):
-            # print("Cached data being used...")
-            # increment count
-            red.incr(f'count:{url}')
-            return red.get(url).decode('utf-8')
-        else:
-            # print("Fetching the new data ...")
-            result = fn(url)
-            # cache the result for 10 seconds
-            red.set(url, result, ex=10)
-            # initialize the count from 1
-            red.set(f'count:{url}', 1)
-            return result
-    return wrapper
-
-
-@cache
 def get_page(url: str) -> str:
-    """Fetch the HTML content of the url"""
+    """Fetch the HTML content of the url and cache value"""
+
+    red.set(f'cached:{url}', count)
     response = requests.get(url)
+    red.incr(f'count:{url}')
+    get = red.get(f'cached:{url}')
+    red.setex(f'cached:{url}', 10, get)
     return response.text
 
-# test the function
-# print(get_page('http://slowwly.robertomurray.co.uk'))
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
