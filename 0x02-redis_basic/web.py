2ddb03881fd4a321c0aa62b2cea2b9327db5c091
uelkariuki <cratzuel@gmail.com>
2023-12-23 00:07:15 +0300

    Fix Count increments when get_page is called

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,18 +9,17 @@ import redis
 
 # connecting to local Redis server
 red = redis.Redis()
-count = 0
 
 
 def get_page(url: str) -> str:
     """Fetch the HTML content of the url and cache value"""
 
-    red.set(f'cached:{url}', count)
-    response = requests.get(url)
-    red.incr(f'count:{url}')
-    red.setex(f'cached:{url}', 10, red.get(f'cached:{url}'))
-    return response.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    cached = red.get(f'cached:{url}')
+    if cached is not None:
+        red.incr(f'count:{url}')
+        return cached.decode('utf-8')
+    else:
+        response = requests.get(url)
+        red.setex(f'cached:{url}', 10, response.text)
+        red.set(f'cached:{url}', 1)
+        return response.text
